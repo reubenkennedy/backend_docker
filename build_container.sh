2814ea05cb86014938a705292eb046e196ba479b
Reuben Kennedy <reubenkennedy@pm.me>
2024-01-27 17:12:36 -0500

    fixed tar file names

--- build_container.sh
+++ build_container.sh
@@ -4,12 +4,12 @@ cp /data/dqs_links/*.so .
 chmod 755 *.so
 if [ "$1" = "aws" ]
 then
-    docker build -t gh_aws --ssh default -f Dockerfile.amd64_aws . && docker save -o gw_aws.$dt.tar gh_aws  && gzip gw_aws.$dt.tar
+    docker build -t gh_aws --ssh default -f Dockerfile.amd64_aws . && docker save -o gh_aws.$dt.tar gh_aws  && gzip gh_aws.$dt.tar
 elif [ "$1" = "nonX86" ]
 then
-    docker build -t gh_nonX86 --ssh default -f Dockerfile.nonX86. && docker save -o gw_nonx86.$dt.tar gh_nonX86 && gzip gw_nonx86.$dt.tar
+    docker build -t gh_nonX86 --ssh default -f Dockerfile.nonX86. && docker save -o gh_nonx86.$dt.tar gh_nonX86 && gzip gh_nonx86.$dt.tar
 else
-    docker build -t gh_debian --ssh default -f Dockerfile.amd64 . && docker save -o gw_debian.$dt.tar gh_debian && gzip gw_debian.$dt.tar
+    docker build -t gh_debian --ssh default -f Dockerfile.amd64 . && docker save -o gh_debian.$dt.tar gh_debian && gzip gh_debian.$dt.tar
 fi
 
 if [ $? = 0 ]
