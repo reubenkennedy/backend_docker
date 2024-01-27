9f8c360df23b9bcd1662f8d13fddf9575bfa5b24
Reuben Kennedy <reubenkennedy@pm.me>
2024-01-27 17:42:21 -0500

    shell script stuff for nonx86

--- build_container.sh
+++ build_container.sh
@@ -7,7 +7,7 @@ then
     docker build -t gh_aws --ssh default -f Dockerfile.amd64_aws . && docker save -o gh_aws.$dt.tar gh_aws  && gzip gh_aws.$dt.tar
 elif [ "$1" = "nonX86" ]
 then
-    docker build -t gh_nonX86 --ssh default -f Dockerfile.nonX86. && docker save -o gh_nonx86.$dt.tar gh_nonX86 && gzip gh_nonx86.$dt.tar
+    docker build -t gh_nonx86 --ssh default -f Dockerfile.nonX86 . && docker save -o gh_nonx86.$dt.tar gh_nonx86 && gzip gh_nonx86.$dt.tar
 else
     docker build -t gh_debian --ssh default -f Dockerfile.amd64 . && docker save -o gh_debian.$dt.tar gh_debian && gzip gh_debian.$dt.tar
 fi
