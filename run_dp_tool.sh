a039c4a9a0da0dbe660f1353f33415af73b96af7
Reuben Kennedy <reubenkennedy@pm.me>
2024-01-27 17:17:17 -0500

    added run_dp_tool.sh

---
+++ run_dp_tool.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# --rm = cleanup container on exit
+# --mount = mounts local host dirs into the container
+# --env
+docker run --rm --env RUST_LOG=info --mount type=bind,src=/data,dst=/data -it gh_debian dp_tool $*
