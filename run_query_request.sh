e87536edfb4aba2fb47780e37198843e0a6235c4
Reuben Kennedy <reubenkennedy@pm.me>
2024-02-02 11:33:39 -0500

    testing mods on scripts

--- run_query_request.sh
+++ run_query_request.sh
@@ -3,4 +3,12 @@
 # -p = bind loopback:3000 to the container 3000
 # --mount = mounts local host dirs into the container
 # --env
-docker run --rm --network container:backend --env JWT_SECRET="secret" --env RUST_LOG=info --mount type=bind,src=.,dst=/data -it gh-queryserver query_tool send -t token.json -u "ws://127.0.0.1:3000/ws" $*
+if [ "$1" = "app-stack" ]
+then
+    # if you want to run against the app-stack container use this one
+    docker run --rm --network=host --env JWT_SECRET="secret" --env RUST_LOG=info --mount type=bind,src=.,dst=/data -it gh-queryserver query_tool send -t token.json -u "ws://127.0.0.1:4002/ws" $*
+else 
+    # else you are running against one fired up with run_query_server.sh
+    docker run --rm --network container:backend --env JWT_SECRET="secret" --env RUST_LOG=info --mount type=bind,src=.,dst=/data -it gh-queryserver query_tool send -t token.json -u "ws://127.0.0.1:3000/ws" $*
+
+fi
